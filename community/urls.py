a08019b0b338a406bf6200667b127f92e7b54c93
9yuhyeon <kkh2742@gmail.com>
2022-11-11 11:09:47 +0900

    add articleview and create_article function

--- community/urls.py
+++ community/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from community import views
+
+app_name = 'community'
 
 urlpatterns = [
-
+    path('', views.index, name='index'), # 게시글 전체 보기
+    path('create_article/', views.create_article, name='create_article'), # 게시글 작성
 ]
--- community/views.py
+++ community/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.all().order_by('-created_at')
+    context = {
+        'articles':articles
+    }
+    return render(request, 'index.html', context)
+
+
+def create_article(request):
+    if request.method == 'GET':
+        return render(request, 'create_article.html')
+    elif request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Article.objects.create(title=title, content=content, user=request.user)
+        return redirect('community:index')
